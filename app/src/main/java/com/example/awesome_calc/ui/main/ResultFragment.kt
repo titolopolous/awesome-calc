2b90fa2cee67346efb1fd7af560781949a2648c6
Cesar A. Burkard <augustosaca@outlook.com>
2023-04-09 21:05:39 +0200

    Sharing implemented.

--- app/src/main/java/com/example/awesome_calc/ui/main/ResultFragment.kt
+++ app/src/main/java/com/example/awesome_calc/ui/main/ResultFragment.kt
@@ -1,8 +1,7 @@
 package com.example.awesome_calc.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,20 +10,35 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import com.example.awesome_calc.R
+import kotlin.properties.Delegates
 
 class ResultFragment(val viewModel: MainViewModel) : Fragment() {
+    private var result by Delegates.notNull<Double>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment_result, container, false)
-        var result = viewModel.getOperationResult()
+        result = viewModel.getOperationResult()
 
         view.findViewById<TextView>(R.id.resultTextView).text = result.toString()
+        view.findViewById<Button>(R.id.shareButton).setOnClickListener { shareResult() }
         view.findViewById<Button>(R.id.continueButton).setOnClickListener { doContinue() }
 
         viewModel.clear()
         return view
+    }
+
+    fun shareResult() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, result.toString())
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, getString(R.string.share_title))
+        startActivity(shareIntent)
     }
 
     fun doContinue() {
