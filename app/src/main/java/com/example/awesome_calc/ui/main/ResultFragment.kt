d7a4d3102688c294462a0ebb2b5873db1c733e08
Cesar A. Burkard <augustosaca@outlook.com>
2023-04-09 20:53:39 +0200

    Continue button functionality implemented.

--- app/src/main/java/com/example/awesome_calc/ui/main/ResultFragment.kt
+++ app/src/main/java/com/example/awesome_calc/ui/main/ResultFragment.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
 import com.example.awesome_calc.R
 
 class ResultFragment(val viewModel: MainViewModel) : Fragment() {
@@ -19,8 +21,14 @@ class ResultFragment(val viewModel: MainViewModel) : Fragment() {
         var result = viewModel.getOperationResult()
 
         view.findViewById<TextView>(R.id.resultTextView).text = result.toString()
+        view.findViewById<Button>(R.id.continueButton).setOnClickListener { doContinue() }
 
         viewModel.clear()
         return view
+    }
+
+    fun doContinue() {
+        var fm = (activity as FragmentActivity).supportFragmentManager;
+        fm.popBackStackImmediate()
     }
 }
